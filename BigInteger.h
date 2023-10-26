4f392891c97c8e045fda5b97979680f6a4dadc24
Kshitij Tyagi <kshitijtyagiwastaken@gmail.com>
2023-10-26 18:46:41 +0530

    Update BigInteger.h

--- BigInteger.h
+++ BigInteger.h
@@ -1,3 +1,9 @@
+#ifndef BIG_INTEGER_H
+#define BIG_INTEGER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 struct node
 {
     unsigned int data : 4;
@@ -16,3 +22,5 @@ struct BigInteger add(struct BigInteger,struct BigInteger);
 struct BigInteger sub(struct BigInteger,struct BigInteger);
 struct BigInteger div1(struct BigInteger,struct BigInteger);
 struct BigInteger mod(struct BigInteger,struct BigInteger);
+
+#endif // BIG_INTEGER_H
